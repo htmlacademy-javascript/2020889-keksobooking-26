9a6d6699fc9c27130e05edbb8ce028351304db1c
Kamilla <artkova.kamilla@gmail.com>
2022-06-06 19:09:25 +0900

    Uploaded new functions

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ function getRandomPositiveInteger (a, b) {
 
     return Math.round(result);
   }
-console.log(getRandomPositiveInteger(1,100))
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 function getRandomPositiveFloat (a, b, digits) {
@@ -16,4 +15,3 @@ function getRandomPositiveFloat (a, b, digits) {
 
     return +result.toFixed(digits);
   }
-  console.log(getRandomPositiveFloat(1,100,6))
