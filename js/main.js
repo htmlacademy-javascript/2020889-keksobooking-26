4530d4c3c9451ff9d25f3f6690744064f7249ac9
Kamilla <artkova.kamilla@gmail.com>
2022-06-15 23:06:59 +0900

    Made a random slice of feautures

--- js/main.js
+++ js/main.js
@@ -78,46 +78,43 @@ function getRandomPositiveFloat (a, b, digits) {
 }
 
 const getRandomPictureLink = () => {
-  let result = getRandomPositiveInteger(1, 10);
-  if (result !== 10) {
-    result = `0${result}`;
-  }
-  return result;
+  const result = getRandomPositiveInteger(1, 10);
+  return result < 10 ? `0${result}` : result;
 };
 
-const getLocationLat = getRandomPositiveFloat(MINIMUM_LAT, MAXIMUM_LAT, LOCATION_NUMBER_DIGITS);
-
-const getLocationLng = getRandomPositiveFloat(MINIMUM_LNG, MAXIMUM_LNG, LOCATION_NUMBER_DIGITS);
 
 const getRandomArrayElement = (elements) =>
   elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-const createPost = () => ({
-  author: {
-    avatar: `img/avatars/user${getRandomPictureLink()}.png`
-  },
-  offer: {
-    title: getRandomArrayElement(TITLES),
-    address: {
+const createPost = () => {
+  const getLocationLat = getRandomPositiveFloat(MINIMUM_LAT, MAXIMUM_LAT, LOCATION_NUMBER_DIGITS);
+  const getLocationLng = getRandomPositiveFloat(MINIMUM_LNG, MAXIMUM_LNG, LOCATION_NUMBER_DIGITS);
+  return {
+    author: {
+      avatar: `img/avatars/user${getRandomPictureLink()}.png`
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: {
+        lat: getRandomPositiveFloat(MINIMUM_LAT, MAXIMUM_LAT, LOCATION_NUMBER_DIGITS),
+        lng: getRandomPositiveFloat(MINIMUM_LNG, MAXIMUM_LNG, LOCATION_NUMBER_DIGITS),
+      },
+      price: getRandomPositiveInteger(MINIMUM_PRICE, MAXIMUM_PRICE),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomPositiveInteger(MINIMUM_ROOM_NUMBER, MAXIMUM_ROOM_NUMBER),
+      guests: getRandomPositiveInteger(MINIMUM_GUESTS_NUMBER, MAXIMUM_GUESTS_NUMBER),
+      checkin: getRandomArrayElement(CHECKIN),
+      checkout: getRandomArrayElement(CHECKOUT),
+      features: FEATURES.slice(0, getRandomPositiveInteger(0, FEATURES.length)),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: getRandomArrayElement(PHOTOS),
+    },
+    location: {
       lat: getLocationLat,
       lng: getLocationLng,
     },
-    price: getRandomPositiveInteger(MINIMUM_PRICE, MAXIMUM_PRICE),
-    type: getRandomArrayElement(TYPES),
-    rooms: getRandomPositiveInteger(MINIMUM_ROOM_NUMBER, MAXIMUM_ROOM_NUMBER),
-    guests: getRandomPositiveInteger(MINIMUM_GUESTS_NUMBER, MAXIMUM_GUESTS_NUMBER),
-    checkin: getRandomArrayElement(CHECKIN),
-    checkout: getRandomArrayElement(CHECKOUT),
-    features: getRandomArrayElement(FEATURES),
-    description: getRandomArrayElement(DESCRIPTIONS),
-    photos: getRandomArrayElement(PHOTOS),
-  },
-  location: {
-    lat: getLocationLat,
-    lng: getLocationLng,
-  },
-});
+  };
+};
 
 const similarPosts = Array.from({length: SIMILAR_POSTS_COUNT}, createPost);
 similarPosts();
-
