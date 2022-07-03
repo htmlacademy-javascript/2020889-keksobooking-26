905360dbf2a1b0a2e691216f1a47b397fed343b9
Kamilla <artkova.kamilla@gmail.com>
2022-07-03 12:41:58 +0900

    Adeed a new module to main

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import './util.js';
 import './generating-similar-elements.js';
-import './dealing-with-form';
+import './dealing-with-form.js';
 import {createPost, onePost} from './generating-similar-elements.js';
 createPost(onePost);
