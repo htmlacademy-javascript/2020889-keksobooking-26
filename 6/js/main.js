8b2f9330a12f54941059507e4454cb3f120bada2
Keks <keks@htmlacademy.ru>
2022-07-04 09:42:14 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,5 @@
+import './util.js';
+import './generating-similar-elements.js';
+import './dealing-with-form.js';
+import {createPost, onePost} from './generating-similar-elements.js';
+createPost(onePost);
