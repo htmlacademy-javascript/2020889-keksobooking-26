91fa1b3416da90e5a737017f241411849f6323b1
Kamilla <artkova.kamilla@gmail.com>
2022-07-06 01:15:14 +0900

    Created user-form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import './util.js';
 import './generating-similar-elements.js';
 import './dealing-with-form.js';
+import './user-form.js';
 import {createPost, onePost} from './generating-similar-elements.js';
 createPost(onePost);
+
--- js/user-form.js
+++ js/user-form.js
@@ -1,3 +1,72 @@
-//proishodit vibor iz spiska
-//avtomatichecki value=min znacheniyu
-//esli znacheniye menwe vidaet owibku
+const orderForm = document.querySelector('.ad-form');
+
+const pristine = new Pristine(orderForm, {
+  classTo: 'ad-form__element', // Элемент, на который будут добавляться классы
+  errorClass: 'form__error', // Класс, обозначающий невалидное поле
+  successClass: 'form__success', // Класс, обозначающий валидное поле
+  errorTextParent: 'ad-form__element', // Элемент, куда будет выводиться текст с ошибкой
+  errorTextTag: 'span', // Тег, который будет обрамлять текст ошибки
+  errorTextClass: 'form__error' // Класс для элемента с текстом ошибки
+});
+
+//3.1. Заголовок объявления:
+const titleField = orderForm.querySelector('#title');
+
+const validateTitleLength = (value) => value.length >= 30 && value.length <= 100;
+
+pristine.addValidator(
+  titleField,
+  validateTitleLength,
+  'Заголовок должен быть от 30 до 100 символов',
+);
+
+//3.2. Цена за ночь:
+const priceField = orderForm.querySelector('#price');
+const validatePriceValue = (value) => value <= 100000;
+
+pristine.addValidator(
+  priceField,
+  validatePriceValue,
+  'Цена должна быть меньше 100,000',
+);
+
+//3.6. Поле «Количество комнат» синхронизировано с полем «Количество мест» таким образом,
+//что при выборе количества комнат вводятся ограничения на допустимые варианты выбора количества гостей:
+const capacityField = orderForm.querySelector('#capacity');
+const roomNumberField = orderForm.querySelector('#room_number');
+
+const maxCapacity = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0']
+};
+
+const maxCapacityErrorMessage = {
+  '1': 'для 1 гостя',
+  '2': 'для 1-2 гостей',
+  '3': 'для 1-3 гостей',
+  '100': 'не для гостей',
+};
+
+const validateCapacityField = (value) => maxCapacity[roomNumberField.value].includes(value);
+
+const dontValidateCapacityField = () => `${maxCapacityErrorMessage[roomNumberField.value]}`;
+
+pristine.addValidator(
+  capacityField,
+  validateCapacityField,
+  dontValidateCapacityField,
+);
+
+//При выборе комнаты, автоматом проставляется число людей
+function changeRoomNumber () {
+  pristine.validate(capacityField);
+}
+
+roomNumberField.addEventListener('change', changeRoomNumber);
+
+orderForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
